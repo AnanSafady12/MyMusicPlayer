913322a9575ac9ef96914e16636cd8b747ab9bbd
anan safady <anansafady73@gmail.com>
2026-02-11 01:26:51 +0200

    Add async iTunes metadata fetch with cancellation and default cover

---
+++ MyMusicPlayer/Models/ITunesModels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MyMusicPlayer.Models
+{
+    public class ITunesSearchResponse
+    {
+        [JsonPropertyName("resultCount")]
+        public int ResultCount { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<ITunesTrack> Results { get; set; } = new();
+    }
+
+    public class ITunesTrack
+    {
+        [JsonPropertyName("trackName")]
+        public string? TrackName { get; set; }
+
+        [JsonPropertyName("artistName")]
+        public string? ArtistName { get; set; }
+
+        [JsonPropertyName("collectionName")]
+        public string? CollectionName { get; set; }
+
+        // artworkUrl100 exists almost always; we can convert to higher res later
+        [JsonPropertyName("artworkUrl100")]
+        public string? ArtworkUrl100 { get; set; }
+    }
+}
